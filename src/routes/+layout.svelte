428e78feb98cd10097069060372b30197cb73972
AnimeStudioInc <animestudioinc18@gmail.com>
2026-03-13 15:33:08 +0800

    adding SEO HTMl tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import "../app.css";
 	import Footer from "../components/Footer.svelte";
   </script>
+
+  <svelte:head>
+  <meta name="google-site-verification" content="HuiXxKnmAYLI70Dp4-MAKKTt-L22a7uaOnP1H1W--mg" />
+  </svelte:head>
   
   <slot />
   <Footer/>
